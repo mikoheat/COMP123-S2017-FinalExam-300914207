f246569ab78918de570fa9631e45d5037b610e76
300914207 <300914207@PR-A315-28>
2017-08-17 13:10:10 -0400

    Add project files.

---
+++ COMP123-S2017-FinalExam-300914207/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+* Name: Taeho Kim
+* Date: August 17, 2017
+* StudentID: 300914207
+* Description: PickHighestCard Demo Project
+* Version: 0.1 - Create reference to Form
+*/
+
+namespace COMP123_S2017_FinalExam_300914207
+{
+    public static class Program
+    {
+        // Create reference to Form
+        public static PickHighestCardForm pickcard;
+
+        /// <summary>
+        /// This runs application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplashForm());
+        }
+    }
+}
---
+++ COMP123-S2017-FinalExam-300914207/SplashForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+* Name: Taeho Kim
+* Date: August 17, 2017
+* StudentID: 300914207
+* Description: PickHighestCard Demo Project
+* Version: 0.1 - Create a SplashForm and implement SplashFormTimer_Tick event handler.
+*/
+
+namespace COMP123_S2017_FinalExam_300914207
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {
+            
+
+            this..Show();
+
+            this.Hide();
+
+            SplashFormTimer.Enabled = false;
+        }
+    }
+}
