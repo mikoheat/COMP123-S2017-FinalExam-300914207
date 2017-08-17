18ec24e69d4c69f0ebdb838845a4b3acadd15f27
300914207 <300914207@PR-A315-28>
2017-08-17 13:15:16 -0400

    Create ScoreBoard class

---
+++ COMP123-S2017-FinalExam-300914207/ScoreBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Name: Taeho Kim
+* Date: August 17, 2017
+* StudentID: 300914207
+* Description: PickHighestCard Demo Project
+* Version: 0.1 - Create ScoreBoard class
+*/
+
+namespace COMP123_S2017_FinalExam_300914207
+{
+    class ScoreBoard
+    {
+    }
+}
